4c921b8fc0d5a9a033528f03d79e78746a4294af
VEERESH HEDDERI <163425181+veeresh-2004@users.noreply.github.com>
2025-09-15 18:23:32 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-// allow frontend (React) to call backend
+// allow frontend (React) to casll backend
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true
